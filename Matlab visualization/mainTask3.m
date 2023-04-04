1f71dd2a72abeb1f70915e9bc5ce5d781f30275e
Lars Gehrmann <94363869+LarsGehrmann@users.noreply.github.com>
2023-04-04 18:40:15 +0200

    Create mainTask3.m

---
+++ Matlab visualization/mainTask3.m
@@ -0,0 +1,15 @@
+% Task3, starting with sampling of positrons
+
+clear all, clc, close all;
+
+%sample();
+%plot_scaleB();
+%plot_distTargetOrigin();
+%plot_Annihis();
+plot_sampleWalls()
+%plot_Sample4distTargetOrigin()
+%plot_Sample4ModeratorHeight()
+%plot_Sample4Fields()
+%plot_scaleBTargetHits()
+%plot_scaleBDipoleHits()
+%plot_scaleBNeonHits()
